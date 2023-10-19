4ff1bdb5336f60e18c96b346bf1dcc87b4ccd76e
root <root@be369dc061be>
2023-10-19 01:24:36 +0000

    Finished up to Robot in Space

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,23 @@
-
+PImage olive;
+PImage mushroom;
 void setup() {
-    
+  size(500, 500); 
+  fill(#E8CF10);
+  ellipse(250, 250, 250, 250);
+  fill(#FAF208);
+  ellipse(250, 250, 225, 225);
+  fill(#FF6200);
+  ellipse(250, 250, 200, 200);
+  olive = loadImage("olive.png");
+  olive.resize(50, 50);
+  mushroom = loadImage("mushroom.png");
+  mushroom.resize(50, 50);
 }
 void draw() {
-    
+  if (mousePressed && (mouseButton == LEFT)) {
+    image(olive, mouseX-25, mouseY-25);
+  }
+  if (mousePressed && (mouseButton == RIGHT)) {
+     image(mushroom, mouseX-25, mouseY-25);
+  }  
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,51 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		int score=0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String riddle = JOptionPane.showInputDialog("What can travel around the world while staying in a corner?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if(riddle.equals("stamp")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		}
+		
+		riddle = JOptionPane.showInputDialog("What has to be broken before you use it?");
+		if(riddle.equals("egg")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		}
+		
+		riddle = JOptionPane.showInputDialog("What runs all around a backyard, yet never moves?");
+		if(riddle.equals("fence")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		}
+		
+		riddle = JOptionPane.showInputDialog("I am an odd number. Take away a letter and I become even. What number am I?");
+		if(riddle.equals("seven")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, "Score = " + score);
 	}
 }
 
